e18a70875faf1aa2ae981adc54a7f77c1590c99c
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-08-15 15:14:01 -0300

    Update..

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -1,4 +1,18 @@
 package one.digitalinnovation.gof.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter
+    @Getter
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private String endereco;
 }
--- src/main/java/one/digitalinnovation/gof/model/Endereco.java
+++ src/main/java/one/digitalinnovation/gof/model/Endereco.java
@@ -1,4 +1,32 @@
 package one.digitalinnovation.gof.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Os atributos desse modelo foram gerados automaticamente pelo site
+ * jsonschema2pojo.org. Usando um JSON de retorno da API do VIACEP.
+ *
+ * @author AlcidesThiago2016
+ */
+
+@Entity
 public class Endereco {
+
+    @Id
+    @Setter
+    @Getter
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 }
